553cf2f4274d1391b8aaa12ac067a2c36e0cc31a
Sahil Ambure <ambures87@gmail.com>
2023-05-25 11:08:42 +0530

    init

---
+++ android/app/src/main/kotlin/com/example/smarthomeapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smarthomeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const containerColor = Color(0xFFfcf3e8);
+  static const textColor = Color(0xFF56473e);
+  static const secondContainer = Color(0xFFfac194);
+  static const thirdContainer = Color(0xFFf47f76);
+  static const bottomNavBar = Color(0xFF4faf9c);
+}
